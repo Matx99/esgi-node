fa7a04549e93a9329339cf96cc6b3f3750a8ff5b
Karl MARQUES <kmarques@vetixy.com>
2021-05-10 17:23:27 +0200

    4IW3: express + mongoose

--- 4IW3/server/server.js
+++ 4IW3/server/server.js
@@ -1,21 +1,39 @@
+const express = require("express");
 const HttpCode = require("./models/mongo/http_code");
 
-const { MongooseGenerator, Scrapper } = require("./scrapper");
+const app = express();
 
-new Scrapper(
-  { url: "https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP" },
-  (document) => {
-    const trs = [];
-    document.querySelectorAll(".wikitable tbody tr").forEach((trElement) => {
-      if (trElement.querySelector('th[scope="col"]')) return;
-      const fields = trElement.querySelectorAll("th,td");
-      trs.push({
-        _id: fields[0].textContent.trim(),
-        message: fields[1].textContent.trim(),
-        description: fields[2].textContent.trim(),
-      });
-    });
-    return trs;
-  },
-  (data) => MongooseGenerator(data, HttpCode)
-).scrap();
+app.use(express.json());
+
+app.get("/codes", (req, res) => {
+  const { page, perPage, ...query } = req.query;
+  HttpCode.find(query)
+    .limit(parseInt(perPage))
+    .skip((parseInt(page) - 1) * parseInt(perPage))
+    .then((data) => res.json(data));
+});
+
+app.post("/codes", (req, res) => {
+  HttpCode.create(req.body).then((data) => res.status(201).json(data));
+});
+
+app.get("/codes/:code", (req, res) => {
+  const id = req.params.code;
+  HttpCode.findById(id).then((data) =>
+    data !== null ? res.json(data) : res.sendStatus(404)
+  );
+});
+app.put("/codes/:code", (req, res) => {
+  HttpCode.findByIdAndUpdate(req.params.code, req.body, {
+    new: true,
+  }).then((data) => (data !== null ? res.json(data) : res.sendStatus(404)));
+});
+
+app.delete("/codes/:code", (req, res) => {
+  // <==> HttpCode.deleteOne({ _id: req.params.code })
+  HttpCode.findByIdAndDelete(req.params.code).then((data) =>
+    res.sendStatus(data !== null ? 204 : 404)
+  );
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("server is listening"));
